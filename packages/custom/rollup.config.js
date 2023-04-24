df1b846c6091cc63390adb60612ccab2a7c4769b
ouyangchao <ouyangchao@functorz.com>
2023-04-24 15:10:28 +0800

    chore: update build config

--- packages/custom/rollup.config.js
+++ packages/custom/rollup.config.js
@@ -6,7 +6,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'bin/bundle.js',
+    file: 'dist/bundle.js',
     format: 'cjs',
   },
   plugins: [typescript({ sourceMap: false }), nodeResolve(), commonjs(), json()],
