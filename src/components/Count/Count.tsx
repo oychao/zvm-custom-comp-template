3ede1ba3709111293c09c6448a6a6fe494c2adfd
ouyangchao <ouyangchao@functorz.com>
2023-08-16 13:26:31 +0800

    feat: demo

--- src/components/Count/Count.tsx
+++ src/components/Count/Count.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-empty-pattern */
 import { useState } from 'react';
 
 interface CountProps {
   globalData: Record<string, any>;
 }
 
-export function Count({}: CountProps) {
+export function Count(props: CountProps) {
+  console.log(props);
   const [count, setCount] = useState(0);
   return <button onClick={() => setCount(count + 1)}>{count}</button>;
 }
